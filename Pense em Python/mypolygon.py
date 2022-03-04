6e9a24a9addc0780418f9223c558df81ea2d15eb
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-03 23:11:02 -0300

    adicionando exercicios

---
+++ Pense em Python/mypolygon.py
@@ -0,0 +1,62 @@
+import turtle
+# bob = turtle.Turtle()
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# print(bob)
+# turtle.mainloop()
+
+# bob2 = turtle.Turtle()
+# for i in range(4):
+#     bob2.fd(100)
+#     bob2.lt(90)
+# print(bob2)
+# turtle.mainloop()
+
+# import turtle
+# bob = ''
+
+# def square(t):
+#     t = turtle.Turtle()
+#     for i in range(4):
+#         t.fd(100)
+#         t.lt(90)
+#     return t
+# desenho = square(bob)
+# print(desenho)
+# turtle.mainloop()
+
+# import turtle
+# bob = ''
+
+# def square(t, length):
+#     t = turtle.Turtle()
+#     for i in range(4):
+#         t.fd(length)
+#         t.lt(90)
+#     return t
+# desenho1 = square(bob, 25)
+# desenho2 = square(bob, 50)
+# desenho3 = square(bob, 75)
+# desenho4 = square(bob, 100)
+# desenho5 = square(bob, 125)
+# desenho6 = square(bob, 150)
+# print(desenho1)
+# print(desenho2)
+# print(desenho3)
+# print(desenho4)
+# print(desenho5)
+# print(desenho6)
+
+
+for i in range(10):
+    turtle.right(45)
+    turtle.forward(100)
+    turtle.left(360)
+    turtle.forward(5)
+
+
